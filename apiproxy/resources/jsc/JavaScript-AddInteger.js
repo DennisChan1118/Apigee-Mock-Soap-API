ce60e0a6508700f5b1ae17a6f7e037d6c674f39f
Dennis Chan <windyskykarl912@gmail.com>
2020-09-02 18:55:52 +0800

    Update variable name.

--- apiproxy/resources/jsc/JavaScript-AddInteger.js
+++ apiproxy/resources/jsc/JavaScript-AddInteger.js
@@ -1,12 +1,12 @@
-var num1 = context.getVariable("flow.input.num1");
+var num1 = context.getVariable("cv.input.num1");
 if (num1 == null) {
     num1 = 0;
 }
 
-var num2 = context.getVariable("flow.input.num2");
+var num2 = context.getVariable("cv.input.num2");
 if (num2 == null) {
     num2 = 0;
 }
 
 var answer = parseInt(num1) + parseInt(num2);
-context.setVariable("flow.output.answer", answer.toString());
+context.setVariable("cv.output.answer", answer.toString());
